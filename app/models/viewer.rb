76e7519bb230a8ec3e5a076a30d618c4899310ce
github username <github email address>
2019-08-13 10:43:42 -0400

    I had two methods/deliverables left :( I can talk through if you would like

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,35 @@ class Viewer
   def self.all
     @@all
   end
+
+
+  def queue_items
+    #returns an array of QueueItem instances associated with this instance of viewer 
+    QueueItem.all.select do |item|
+      item.viewer == self
+    end
+
+  end
+
+  def queue_movies
+    #should return an array of Movie instances in the viewers queue
+
+    self.queue_items.map do |item|
+      item.movie
+    end
+
+  end
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(self, movie)
+  end
+
+  def rate_movie(movie, rating)
+    if self.queue_movies.include?(movie)
+      self.queue_items.rating = rating
+    end
+    binding.pry
+    QueueItem.new(self, movie, rating)
+  end
   
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,5 +8,35 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+ryan = Viewer.new("ryan")
+dan = Viewer.new("dan")
+tracie = Viewer.new("tracie")
+dana = Viewer.new("dana")
+gabbie = Viewer.new("gabbie")
+evans = Viewer.new("evans")
+steven = Viewer.new("steven")
+justin = Viewer.new("justin")
+
+godfather = Movie.new("The Godfather")
+toy_story = Movie.new("Toy Story")
+shrek = Movie.new("Shrek")
+inception = Movie.new("Inception")
+ferris_bueller = Movie.new("Ferris Bueller's Day Off")
+antz = Movie.new("Antz")
+lalaland = Movie.new("La La Land")
+
+
+item1 = QueueItem.new(ryan, ferris_bueller)
+item2 = QueueItem.new(dan, toy_story)
+item3 = QueueItem.new(gabbie, toy_story)
+item4 = QueueItem.new(evans, inception)
+item5 = QueueItem.new(dana, lalaland)
+item6 = QueueItem.new(ryan, inception)
+item7 = QueueItem.new(dan, godfather)
+item8 = QueueItem.new(steven, shrek)
+item9 = QueueItem.new(dan, lalaland)
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
