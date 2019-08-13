87088e740d9f120c3f37873c444db281bd6eff67
github username <github email address>
2019-08-13 10:48:01 -0400

    finished

--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,27 @@
 class QueueItem
 
+    attr_accessor :viewer, :movie, :rating
 
-end
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+end #class end
+
+# QueueItem.all
+#     returns an array of all QueueItems
+# QueueItem#viewer
+#     returns the viewer associated with this QueueItem
+# QueueItem#movie
+#     returns the movie associated with this QueueItem
+# QueueItem#rating
+#     returns the rating for this QueueItem. If the viewer has not yet rated the movie, QueueItem#rating should be nil
--- tools/console.rb
+++ tools/console.rb
@@ -4,9 +4,28 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+amanda = Viewer.new("Amanda")
+marisa = Viewer.new("Marisa")
+danny = Viewer.new("Danny")
+sophia = Viewer.new("Sophia")
+
+
+
+
+
+
+harry_potter = Movie.new("Harry Potter")
+  # viewer.queue_item
+usual_suspects = Movie.new("Usual Suspects")
+the_departed = Movie.new("The Departed")
+toy_story = Movie.new("Toy Story")
+the_godfather = Movie.new("The Godfather")
+
+QueueItem.new(danny, the_departed, 5)
+
+
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
